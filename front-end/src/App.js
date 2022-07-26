90d85052e7f3553f3ce27d326506c256624ca41c
shivraj <shivrajpatil0027@gmail.com>
2022-07-26 07:48:36 +0530

    committed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,15 +4,18 @@ import SignUp from "./components/Authentication/SignUp";
 import Login from "./components/Authentication/Login";
 import WelcomeUser from "./components/HomePage/WelcomeUser";
 import AddUserData from "./components/HomePage/AddUserData";
+import Navbar from "./components/header/Navbar";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <Switch>
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/welcomeuser" component={WelcomeUser} />
           <Route exact path="/adduserdata" component={AddUserData} />
+          <Route exact path="/navbar" component={Navbar} />
         </Switch>
       </BrowserRouter>
     </div> 
